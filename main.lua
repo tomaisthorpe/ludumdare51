002881a7ed353fc10416f5e997cc956bba1582b7
Tom Aisthorpe <tomaisthorpe@gmail.com>
2022-10-01 10:32:25 +0100

    Initial commit

---
+++ main.lua
@@ -0,0 +1,19 @@
+local Gamestate = require("hump.gamestate")
+
+local config = require("config")
+local Game = require("game")
+
+function love.load()
+  Gamestate.registerEvents()
+  love.window.setMode(800, 600)
+  love.graphics.setDefaultFilter("nearest", "nearest")
+  love.window.setFullscreen(config.fullscreen)
+
+  Gamestate.switch(Game)
+  
+  love.window.setTitle("Ludum Dare 51")
+end
+
+function setupWindow()
+  love.window.setMode(800, 600)
+end
